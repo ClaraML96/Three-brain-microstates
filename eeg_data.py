5e833a564c73be21b7a817f680024778ae58be7e
ClaraML96 <clara.mejlhede@hotmail.com>
2026-02-23 22:38:19 +0100

    cleaning repo

--- eeg_data.py
+++
@@ -1,8 +0,0 @@
-import mne
-
-raw = mne.io.read_raw_bdf(
-    "C:\\Users\\clara\\OneDrive - Danmarks Tekniske Universitet\\Skrivebord\\DTU\\Human Centeret Artificial Intelligence\\Thesis\\FG_Data_For_Students\\RawEEGData_1-4\\301.bdf",
-    preload=True
-)
-
-print(raw.info)
